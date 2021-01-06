f507e9be4803c9eab7d0db589490d8ba9d921245
Philip Tzou <philip.npc@gmail.com>
2021-01-06 14:47:22 -0800

    The collaspable context used by <Markdown /> should be preserved while the element existing

--- src/components/markdown/index.js
+++ src/components/markdown/index.js
@@ -66,10 +66,32 @@ export default class Markdown extends React.Component {
     tables: {}
   }
 
+  static getDerivedStateFromProps = (props, state = {}) => {
+    let {children, LoadReferences} = this.props;
+    if (children instanceof Array) {
+      children = children.join('');
+    }
+    if (
+      children !== state.children &&
+      LoadReferences !== state.LoadReferences
+    ) {
+      return {
+        children,
+        LoadReferences,
+        context: new ReferenceContextValue(LoadReferences)
+      };
+    }
+    return null;
+  }
+
+  constructor() {
+    super(...arguments);
+    this.state = this.constructor.getDerivedStateFromProps(this.props);
+  }
+
   render() {
     let {
       noHeadingStyle, toc,
-      children,
       referenceTitle, inline, tocClassName,
       disableHeadingTagAnchor,
       referenceHeadingTagLevel,
@@ -77,12 +99,9 @@ export default class Markdown extends React.Component {
       imagePrefix,
       cmsPrefix,
       tables,
-      LoadReferences,
       renderers: addRenderers, ...props
     } = this.props;
-    if (children instanceof Array) {
-      children = children.join('');
-    }
+    const {children, context} = this.state;
     const mdProps = {
       parserOptions: {footnotes: true},
       transformLinkUri: false,
@@ -110,7 +129,6 @@ export default class Markdown extends React.Component {
       ...addRenderers
     };
     mdProps.renderers = generalRenderers;
-    const context = new ReferenceContextValue(LoadReferences);
     let jsx = (
       <ReferenceContext.Provider value={context}>
         <OrigMarkdown
