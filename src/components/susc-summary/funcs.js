c99345f9b366d7a013d5941d34d15cf2c8f0d986
Philip Tzou <philip.npc@gmail.com>
2021-04-21 12:05:43 -0700

    Add a rule that switch place of SUBSET and OVERLAP

--- src/components/susc-summary/funcs.js
+++ src/components/susc-summary/funcs.js
@@ -1,4 +1,5 @@
 import uniq from 'lodash/uniq';
+import orderBy from 'lodash/orderBy';
 
 
 export function getUniqVariants(hitVariants) {
@@ -13,7 +14,7 @@ export function getRowKey({mutations}) {
 }
 
 const DEFAULT_DISPLAY_2ND_TYPE_MAX_TOTAL = 3;
-const MAX_ALLOWED_OVERLAP_MISS = 4;
+// const MAX_ALLOWED_OVERLAP_MISS = 4;
 
 export function decideDisplayPriority(items) {
   if (items.length === 0) {
@@ -23,41 +24,82 @@ export function decideDisplayPriority(items) {
     items.map(({variantMatchType}) => variantMatchType)
   );
 
-  const defaultType = matchTypes[0];
-  const secondType = matchTypes[1];
+  let defaultType = matchTypes[0];
+  let secondType = matchTypes[1];
   const expandableTypes = matchTypes.slice(1, 3);
-  const shouldDisplay2ndType = (
-    secondType ?
-      items.filter(
-        ({variantMatchType}) => (
-          variantMatchType === secondType
-        )
-      ).length : 0
-  ) <= DEFAULT_DISPLAY_2ND_TYPE_MAX_TOTAL;
   const results = [];
+  let subsetMaxNumMiss = 0;
+  let overlapMaxNumMiss = 0;
+  let hasOverlap = false;
   for (const item of items) {
     const {
       variantMatchType,
       numVariantOnlyMutations,
       numQueryOnlyMutations
     } = item;
+    const numMiss = numVariantOnlyMutations + numQueryOnlyMutations;
     let displayOrder = null;
-    if (variantMatchType === defaultType) {
-      displayOrder = 0;
+
+    if (variantMatchType === 'SUBSET') {
+      subsetMaxNumMiss = (
+        subsetMaxNumMiss > numMiss ? subsetMaxNumMiss : numMiss
+      );
     }
-    else if (shouldDisplay2ndType && variantMatchType === secondType) {
+
+    if (variantMatchType === defaultType) {
       displayOrder = 0;
     }
     else if (expandableTypes.includes(variantMatchType)) {
       displayOrder = 1;
     }
     if (displayOrder !== null && variantMatchType === 'OVERLAP') {
-      const numMiss = numVariantOnlyMutations + numQueryOnlyMutations;
-      if (numMiss >= MAX_ALLOWED_OVERLAP_MISS) {
+      if (numMiss >= subsetMaxNumMiss) {
         displayOrder = null;
       }
+      else {
+        hasOverlap = true;
+        overlapMaxNumMiss = (
+          overlapMaxNumMiss > numMiss ? overlapMaxNumMiss : numMiss
+        );
+      }
     }
-    results.push([item, displayOrder]);
+    results.push([item, displayOrder, numMiss]);
   }
-  return results;
+
+  if (hasOverlap && defaultType === 'SUBSET') {
+    // check if we should switch place of SUBSET and OVERLAP
+    // when OVERLAP have general better results than SUBSET
+    if (overlapMaxNumMiss < subsetMaxNumMiss) {
+      for (const one of results) {
+        if (one[1] === 1) {
+          one[1] = 0;
+        }
+        else if (one[1] === 0) {
+          one[1] = 1;
+        }
+      }
+      defaultType = 'OVERLAP';
+      secondType = 'SUBSET';
+    }
+  }
+
+  const shouldDisplay2ndType = (
+    secondType ?
+      results.filter(
+        ([{variantMatchType}, displayOrder]) => (
+          variantMatchType === secondType && displayOrder !== null
+        )
+      ).length : 0
+  ) <= DEFAULT_DISPLAY_2ND_TYPE_MAX_TOTAL;
+
+  if (shouldDisplay2ndType) {
+    for (const one of results) {
+      if (one[0].variantMatchType === secondType && one[1] !== null) {
+        one[1] = 0;
+      }
+    }
+  }
+
+  // re-order the 2nd display level by numMiss
+  return orderBy(results, [1, 2]);
 }
