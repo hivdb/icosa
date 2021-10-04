8062040fb3ab86504b0e19e5c52f6150ac04364b
Philip Tzou <philip.npc@gmail.com>
2021-10-04 14:12:44 -0700

    HeadingTag auto-scroll only when anchor is not empty (see hivdb/chiro-frontend#21); Also convert HeadingTag to function-style

--- src/components/heading-tags/index.js
+++ src/components/heading-tags/index.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import sleep from 'sleep-promise';
 import {FaLink} from '@react-icons/all-files/fa/FaLink';
 import Children from 'react-children-utilities';
 
@@ -16,104 +15,86 @@ export function getAnchor(elem) {
   if (elem.type === HeadingTag) {
     elem = elem.props.children;
   }
-  return getChildrenText(elem).toLowerCase().replace(/[^\w-]+/g, '.');
+  return getChildrenText(elem)
+    .toLowerCase()
+    .replace(/[^\w-]+/g, '.');
+}
+
+HeadingTag.propTypes = {
+  id: PropTypes.string,
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
+  children: PropTypes.node.isRequired,
+  disableAnchor: PropTypes.bool.isRequired
+};
+
+HeadingTag.defaultProps = {
+  disableAnchor: false
+};
+
+export function HeadingTag({id, level, children, disableAnchor, ...props}) {
+
+  const elemRef = React.useRef();
+
+  const anchor = React.useMemo(
+    () => id ? id : getAnchor(children),
+    [id, children]
+  );
+
+  React.useEffect(
+    () => {
+      if (anchor !== '' && window.location.hash.replace(/^#/, '') === anchor) {
+        setTimeout(() => {
+          const top = (
+            elemRef.current.getBoundingClientRect().top + window.pageYOffset
+          );
+          window.scrollTo(0, top);
+        });
+      }
+    },
+    [anchor]
+  );
+
+  const Tag = `h${level}`;
+  return (
+    <Tag
+     {...props}
+     ref={elemRef}
+     className={style['heading-tag']}
+     id={anchor}>
+      {disableAnchor ? null :
+      <a
+       href={`#${this.anchor}`}
+       className={style['anchor-link']}
+       data-anchor-link="">
+        <FaLink name="linkify" />
+      </a>}
+      {children}
+    </Tag>
+  );
+
 }
 
 
-export class HeadingTag extends React.Component {
-
-  static propTypes = {
-    id: PropTypes.string,
-    level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
-    children: PropTypes.node.isRequired,
-    disableAnchor: PropTypes.bool.isRequired
-  }
-
-  static defaultProps = {
-    disableAnchor: false
-  }
-
-  constructor() {
-    super(...arguments);
-    this.elemRef = React.createRef();
-  }
-
-  get anchor() {
-    const {children, id} = this.props;
-    return id ? id : getAnchor(children);
-  }
-
-  async componentDidMount() {
-    if (window.location.hash.replace(/^#/, '') === this.anchor) {
-      await sleep(0);
-      const top = (
-        this.elemRef.current.getBoundingClientRect().top + window.pageYOffset
-      );
-      window.scrollTo(0, top);
-    }
-  }
-
-  render() {
-    const {level, children, disableAnchor, ...props} = this.props;
-    const Tag = `h${level}`;
-    return (
-      <Tag
-       {...props}
-       ref={this.elemRef}
-       className={style['heading-tag']}
-       id={this.anchor}>
-        {disableAnchor ? null :
-        <a
-         href={`#${this.anchor}`}
-         className={style['anchor-link']}
-         data-anchor-link="">
-          <FaLink name="linkify" />
-        </a>}
-        {children}
-      </Tag>
-    );
-  }
-
+export function H1(props) {
+  return <HeadingTag {...props} level={1} />;
 }
 
-
-export class H1 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={1} />;
-  }
+export function H2(props) {
+  return <HeadingTag {...props} level={2} />;
 }
 
-
-export class H2 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={2} />;
-  }
+export function H3(props) {
+  return <HeadingTag {...props} level={3} />;
 }
 
-
-export class H3 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={3} />;
-  }
+export function H4(props) {
+  return <HeadingTag {...props} level={4} />;
 }
 
-
-export class H4 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={4} />;
-  }
+export function H5(props) {
+  return <HeadingTag {...props} level={5} />;
 }
 
-
-export class H5 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={5} />;
-  }
-}
-
-
-export class H6 extends React.Component {
-  render() {
-    return <HeadingTag {...this.props} level={6} />;
-  }
+export function H6(props) {
+  return <HeadingTag {...props} level={6} />;
 }
